ca6ad2416a0e4faa8ad63983213f35e8f6505c26
SKedar328 <115908982+SKedar328@users.noreply.github.com>
2024-06-28 23:48:20 +0530

    Update Nqueens.java easy to understand

--- Nqueens.java
+++ Nqueens.java
@@ -1,65 +1,69 @@
+import java.util.*;
 
-import java.util.Scanner;
 public class Nqueens {
-    static int solutionCount = 0;
-    static int n;
-    static int[][] chessboard;
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter size of chess board");
-        n = scanner.nextInt();
-        chessboard = new int[n][n];
-        solveNQueens(0);
-        System.out.println("Total solutions: " + solutionCount);
-        scanner.close();
-    }
-
-    public static void printChessboard() {
-        for (int[] row : chessboard) {
-            for (int col : row) {
-                System.out.print(col == 1 ? "Q " : ". ");
+    public static boolean issafe(char board[][], int i, int j) {
+        int r = i;
+        int c = j;
+        while (c >= 0) {
+            if (board[r][c] == 'Q') {
+                return false;
             }
-            System.out.println();
+            c--;
         }
-        System.out.println("-----------------");
-    }
-
-    public static boolean isSafe(int row, int col) {
-        // Check horizontal positions
-        for (int i = col; i >= 0; i--) {
-            if (chessboard[row][i] == 1) return false;
+        c = j;
+        while (c >= 0 && r >= 0) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+            c--;
+            r--;
         }
-        int i = row, j = col;
-        // Check the upper left diagonal
-        while (i >= 0 && j >= 0) {
-            if (chessboard[i][j] == 1) return false;
-            i--;
-            j--;
-        }
-        i = row;
-        j = col;
-        // Check the lower left diagonal
-        while (i < n && j >= 0) {
-            if (chessboard[i][j] == 1) return false;
-            i++;
-            j--;
+        r = i;
+        c = j;
+        while (c >= 0 && r < board.length) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+            c--;
+            r++;
         }
         return true;
     }
 
-    public static void solveNQueens(int currentColumn) {
-        if (currentColumn >= n) return;
-        for (int row = 0; row < n; row++) {
-            if (isSafe(row, currentColumn)) {
-                chessboard[row][currentColumn] = 1;
-                if (currentColumn == n - 1) {
-                    printChessboard();
-                    solutionCount++;
-                }
-                solveNQueens(currentColumn + 1);
-                chessboard[row][currentColumn] = 0; // Backtracking
+    public static void solve(char board[][], int c) {
+        if (c >= board.length) {
+            print(board);
+            System.out.println();
+            return;
+        }
+        for (int i = 0; i < board.length; i++) {
+            if (issafe(board, i, c)) {
+                board[i][c] = 'Q';
+                solve(board, c + 1);
+                board[i][c] = '.';
             }
         }
+
+    }
+
+    public static void print(char board[][]) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char board[][] = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        solve(board, 0);
     }
 }
